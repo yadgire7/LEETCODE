37ee06ac4388dc2799c49b8ce2f996e72c1e560e
“saurabh145yadgire@gmail.com” <saurabh145yadgire@gmail.com>
2023-06-02 16:43:23 -0700

    palindrome_number added

---
+++ palindrome_number.py
@@ -0,0 +1,24 @@
+'''
+Using 2 pointers
+'''
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        else:
+            divisor = 1
+
+            while x >= 10 * divisor:
+                divisor = divisor * 10
+
+        while x:
+            right = x % 10
+            left = x // divisor
+            if left != right:
+                return False
+            else:
+                x = (x % divisor) // 10
+                divisor = divisor / 100
+        return True
--- two_sum.py
+++ two_sum.py
@@ -1,13 +1,32 @@
 '''
 This approach can be improved. Mine beats only 10% of the submissions.
+O(n^2)
 '''
+
+# from itertools import combinations
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         pairs = combinations(nums,2)
+#         for pair in pairs:
+#             if sum(pair) == target:
+#                 if len(set(pair)) ==1:
+#                     return [i for i, x in enumerate(nums) if x == pair[0]]
+#                 return [nums.index(pair[0]),nums.index(pair[1])]
+
+'''
+Approach 2: Using HashMap (beats 65% of the submissions)
+O(n)
+'''
+
+
+
 
 from itertools import combinations
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pairs = combinations(nums,2)
-        for pair in pairs:
-            if sum(pair) == target:
-                if len(set(pair)) ==1:
-                    return [i for i, x in enumerate(nums) if x == pair[0]]
-                return [nums.index(pair[0]),nums.index(pair[1])]
+        hashmap = {}
+        for i in range(len(nums)):
+            if target - nums[i] in hashmap:
+                return [i, hashmap[target - nums[i]]]
+            else:
+                hashmap[nums[i]] = i
