60d9f8ae323582aa18ae4322fd17f10254e35ee6
yadgire7 <47882001+yadgire7@users.noreply.github.com>
2024-06-25 16:46:08 -0700

    Create majority_element.py (one of my best submissions)

---
+++ majority_element.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = sorted(nums)
+        return nums[int(len(nums)/2)]
+        
+        
